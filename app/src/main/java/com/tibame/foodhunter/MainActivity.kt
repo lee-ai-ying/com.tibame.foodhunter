a7b8543599f2de79b3943ce1652ec431109aa892
Andy <andy279788@gmail.com>
2024-10-22 17:29:08 +0800

    SearchResult1

--- app/src/main/java/com/tibame/foodhunter/MainActivity.kt
+++ app/src/main/java/com/tibame/foodhunter/MainActivity.kt
@@ -189,11 +189,11 @@ fun Main(
                     Text(text = destination?.route.toString())
                 }
 
-                    composable(context.getString(R.string.str_search)) {
-                        SearchScreen(
-                            navController
-                        )
-                    }
+                composable(context.getString(R.string.str_search)) {
+                    SearchScreen(
+                        navController
+                    )
+                }
 
 
 
