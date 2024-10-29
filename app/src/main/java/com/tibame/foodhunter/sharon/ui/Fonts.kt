45c0c6e24a2d5a2fe06261906c48c77e3dfc895b
SharonMai <ppp300a@gmail.com>
2024-10-29 16:05:24 +0800

    feat: 新增字體Roboto跟字串資源

--- app/src/main/java/com/tibame/foodhunter/sharon/ui/Fonts.kt
+++ app/src/main/java/com/tibame/foodhunter/sharon/ui/Fonts.kt
@@ -1,2 +1,12 @@
-package com.tibame.foodhunter.sharon.ui
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.tibame.foodhunter.R
 
+val Roboto = FontFamily(
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_thin, FontWeight.Thin)
+)
