d3a30d65c6af86465c54e4eac563ff5dd0895870
SharonMai <ppp300a@gmail.com>
2024-11-01 06:06:09 +0800

    feat: 新增每個頁籤的top bar 並從lab中分離

--- app/src/main/java/com/tibame/foodhunter/sharon/LabComponent.kt
+++ app/src/main/java/com/tibame/foodhunter/sharon/LabComponent.kt
@@ -2,106 +2,38 @@ package com.tibame.foodhunter.sharon
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.outlined.Delete
-import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.tibame.foodhunter.R
-import com.tibame.foodhunter.checkTopBarBackButtonShow
-import com.tibame.foodhunter.checkTopBarNoShow
-import com.tibame.foodhunter.global.TopFunctionBar
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun InsideTopBar(
-    canback:Boolean = true,
-    navController: NavHostController? = null,
-    scrollBehavior: TopAppBarScrollBehavior
-){
-    TopAppBar(
-        modifier = Modifier
-//            .padding(innerPadding)
-//            .fillMaxSize()
-            .background(color = Color.White),
-        scrollBehavior = scrollBehavior,
-        title = {
-            Text(stringResource(R.string.str_member))
-        },
-        navigationIcon = {
-
-            if (canback) {
-                IconButton(onClick = {
-                    navController?.popBackStack()
-                }) {
-                    Icon(
-                        Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.str_back)
-                    )
-                }
-            }
-        },
-        actions = {
-            IconButton(onClick = {}) {
-                Icon(
-                    Icons.Outlined.Search,
-                    contentDescription = stringResource(R.string.str_notice)
-                )
-            }
-            IconButton(onClick = {}) {
-                Icon(
-                    Icons.Filled.MoreVert,
-                    contentDescription = stringResource(R.string.str_chat)
-                )
-            }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.White,
-            titleContentColor = MaterialTheme.colorScheme.primary,
-        )
-    )
-}
 
 
 @Composable
@@ -155,13 +87,10 @@ fun NiaTabRow(
 @Composable
 fun TabBarComponent(
     selectedTab: Int,
-    onTabSelected: (Int) -> Unit
+    onTabSelected: (Int) -> Unit,
+    tabList: List<String>
 ) {
-    val tabList = listOf(
-        stringResource(id = R.string.str_calendar),
-        stringResource(id = R.string.str_note),
-        stringResource(id = R.string.str_favorite)
-    )
+
 
     NiaTabRow(selectedTabIndex = selectedTab) {
         tabList.forEachIndexed { index, title ->
--- app/src/main/java/com/tibame/foodhunter/sharon/NoteEditTopBar.kt
+++ app/src/main/java/com/tibame/foodhunter/sharon/NoteEditTopBar.kt
@@ -1,27 +1,40 @@
 package com.tibame.foodhunter.sharon
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import com.tibame.foodhunter.R
+import com.tibame.foodhunter.sharon.components.DeleteConfirmationDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NoteTopBar(
+fun NoteEditTopBar(
     canback:Boolean = true,
     navController: NavHostController? = null,
     scrollBehavior: TopAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
@@ -29,10 +42,12 @@ fun NoteTopBar(
     ),
     hasTitleInput: Boolean = false,
 ){
+    var showDeleteDialog by remember { mutableStateOf(false) }
+
     TopAppBar(
         scrollBehavior = scrollBehavior,
         title = {
-            Text(stringResource(R.string.str_member))
+//            Text(stringResource(R.string.str_member))
         },
         navigationIcon = {
             if (canback) {
@@ -50,20 +65,25 @@ fun NoteTopBar(
             if (!hasTitleInput) {
                 IconButton(onClick = {}) {
                     Icon(
-                        Icons.Outlined.Close,
+                        modifier = Modifier.background(colorResource(R.color.orange_1st)),
+                        imageVector = Icons.Filled.Close,
                         contentDescription = stringResource(R.string.str_notice)
                     )
                 }
             } else {
-                IconButton(onClick = {}) {
+                IconButton(
+                    onClick = {
+                        showDeleteDialog = true
+                    }  //TODO 刪除警告vm
+                ) {
                     Icon(
-                        Icons.Outlined.CheckCircle,
+                        Icons.Outlined.Delete,
                         contentDescription = stringResource(R.string.str_notice)
                     )
                 }
                 IconButton(onClick = {}) {
                     Icon(
-                        Icons.Filled.MoreVert,
+                        Icons.Filled.Share,
                         contentDescription = stringResource(R.string.str_chat)
                     )
                 }
@@ -74,4 +94,25 @@ fun NoteTopBar(
             titleContentColor = MaterialTheme.colorScheme.primary,
         )
     )
+    // 如果顯示刪除對話框
+    if (showDeleteDialog) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0x80000000)) // 半透明黑色背景
+        ) {
+            DeleteConfirmationDialog(
+                onDeleteConfirmed = {
+                    // 點擊確定刪除後的操作
+                    showDeleteDialog = false
+                    // TODO: 在這裡添加刪除邏輯
+                },
+                onCancel = {
+                    // 點擊取消後隱藏對話框
+                    showDeleteDialog = false
+                }
+            )
+        }
+    }
+
 }
