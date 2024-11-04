22b02761271d70575554d49aea30545ec02bb26a
SharonMai <ppp300a@gmail.com>
2024-11-04 21:13:52 +0800

    feat: 刪除警告視窗符合figma設計

--- app/src/main/java/com/tibame/foodhunter/sharon/components/DeleteConfirmationDialog.kt
+++ app/src/main/java/com/tibame/foodhunter/sharon/components/DeleteConfirmationDialog.kt
@@ -1,8 +1,6 @@
 package com.tibame.foodhunter.sharon.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -10,9 +8,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.tibame.foodhunter.ui.theme.FColor
+import com.tibame.foodhunter.ui.theme.FTypography
+
+@Preview
+@Composable
+fun DeleteConfirmationDialogP(){
+    DeleteConfirmationDialog()
+}
+
 
 //TODO 排版
 @Composable
@@ -22,82 +28,65 @@ fun DeleteConfirmationDialog(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxSize()
+            .width(288.dp)
+            .height(144.dp)
             .wrapContentSize(Alignment.Center)
     ) {
         Column(
             modifier = Modifier
-                .width(280.dp)
                 .background(
                     color = Color.White,
                     shape = RoundedCornerShape(28.dp)
                 )
                 .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center
         ) {
             // 彈出框標題
             Text(
                 text = "確定要刪除？",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier.padding(bottom = 16.dp)
+                style = FTypography.Title1,
+                color = FColor.Dark_80,
+                modifier = Modifier.padding(start = 10.dp)
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
             // 按鈕區域
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                modifier = Modifier.width(200.dp).padding(start = 10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 // 取消按鈕
-                TextButton(
-                    onClick = { onCancel() },
+                OutlinedButton(
                     modifier = Modifier
-                        .weight(1f)
-                        .padding(4.dp)
-                        .background(
-                            color = Color.Transparent,
-                            shape = MaterialTheme.shapes.small
-                        )
-                        .border(
-                            width = 1.dp,
-                            color = Color.Gray,
-                            shape = MaterialTheme.shapes.small
-                        )
+                        .weight(1f),
+                    onClick = { onCancel() },
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        containerColor = Color.White,
+                        contentColor = FColor.Dark_80
+                    ),
                 ) {
-                    Text(
-                        text = "取消",
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
+                    Text(text = "取消",style = FTypography.Label1)
                 }
 
                 Spacer(modifier = Modifier.width(8.dp))
 
                 // 刪除按鈕
-                TextButton(
-                    onClick = { onDeleteConfirmed() },
+                OutlinedButton(
                     modifier = Modifier
-                        .weight(1f)
-                        .padding(4.dp)
-                        .background(
-                            color = Color.Transparent,
-                            shape = MaterialTheme.shapes.small
-                        )
-                        .border(
-                            width = 1.dp,
-                            color = Color.Gray,
-                            shape = MaterialTheme.shapes.small
-                        )
+                        .weight(1f),
+                    onClick = { onDeleteConfirmed() },
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        containerColor = Color.White,
+                        contentColor = FColor.Orange_1st
+                    ),
+
                 ) {
-                    Text(
-                        text = "刪除",
-                        color = Color.Red
-                    )
+                    Text(text = "刪除",style = FTypography.Label1)
                 }
             }
+
         }
     }
 }
