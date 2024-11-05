98751d33023ddd0a4db8c876ac696e6f0c2de3db
MSI\Modern 14 <supaucat@gmail.com>
2024-11-05 23:33:22 +0800

    GroupComposable.kt 修改元件 增加預設顯示的value供搜尋快取用
    GroupCreate.kt
    修正日期選擇器選日期上方日期不會跟著改動
    修改建立揪團的VM行為
    
    GroupDataClass.kt
    加上自動對應序列供gson轉換
    修正價格為下限值,上限值
    修正資料結構
    
    GroupRepository.kt
    新增搜尋快取
    修正搜尋結果的存取
    
    GroupSearch.kt
    修正搜尋揪團介面
    修正搜尋揪團VM行為
    
    GroupSearchResult.kt
    修改搜尋結果顯示內容
    修正返回按鈕位置
    
    GroupViewModel.kt
    新增向後端進行搜尋揪團功能
    新增向後端進行建立揪團功能

--- app/src/main/java/com/tibame/foodhunter/ai_ying/GroupRepository.kt
+++ app/src/main/java/com/tibame/foodhunter/ai_ying/GroupRepository.kt
@@ -1,5 +1,6 @@
 package com.tibame.foodhunter.ai_ying
 
+import android.util.Log
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -9,11 +10,21 @@ object GroupRepository {
     val groupChatList = _groupChatList.asStateFlow()
     private val _groupSearchResult = MutableStateFlow(emptyList<GroupSearchResult>())
     val groupSearchResult = _groupSearchResult.asStateFlow()
+    private val _searchCache = MutableStateFlow(GroupSearchData())
+    val searchCache = _searchCache.asStateFlow()
     init {
-        _groupChatList.update { setTestData() }
-        _groupSearchResult.update { searchFakeData() }
+        //_groupChatList.update { setTestData() }
+        //_groupSearchResult.update { searchFakeData() }
     }
-    private fun searchFakeData():List<GroupSearchResult>{
+    fun updateGroupSearchCache(input: GroupSearchData){
+        _searchCache.update { input }
+    }
+
+    fun updateSearchGroupResult(result: List<GroupSearchResult>) {
+        _groupSearchResult.update { result }
+    }
+
+    private fun searchFakeData(): List<GroupSearchResult> {
         return listOf(
             GroupSearchResult(1),
             GroupSearchResult(2),
@@ -32,26 +43,27 @@ object GroupRepository {
             GroupSearchResult(15),
         )
     }
+
     private fun setTestData(): List<GroupChat> {
         return listOf(
-            GroupChat(1,"進行中",99),
-            GroupChat(1,"GroupChat1",1),
-            GroupChat(2,"GroupChat2",1),
-            GroupChat(3,"GroupChat3",1),
-            GroupChat(4,"GroupChat4",1),
-            GroupChat(5,"GroupChat5",1),
-            GroupChat(6,"GroupChat6",1),
-            GroupChat(7,"GroupChat7",1),
-            GroupChat(8,"GroupChat8",1),
-            GroupChat(9,"已完成",99),
-            GroupChat(9,"GroupChat9",0),
-            GroupChat(10,"GroupChat10",0),
-            GroupChat(11,"GroupChat11",0),
-            GroupChat(12,"GroupChat12",0),
-            GroupChat(13,"GroupChat13",0),
-            GroupChat(14,"GroupChat14",0),
-            GroupChat(15,"GroupChat15",0),
-            GroupChat(16,"GroupChat16",0),
+            GroupChat(1, "進行中", 99),
+            GroupChat(1, "GroupChat1", 1),
+            GroupChat(2, "GroupChat2", 1),
+            GroupChat(3, "GroupChat3", 1),
+            GroupChat(4, "GroupChat4", 1),
+            GroupChat(5, "GroupChat5", 1),
+            GroupChat(6, "GroupChat6", 1),
+            GroupChat(7, "GroupChat7", 1),
+            GroupChat(8, "GroupChat8", 1),
+            GroupChat(9, "已完成", 99),
+            GroupChat(9, "GroupChat9", 0),
+            GroupChat(10, "GroupChat10", 0),
+            GroupChat(11, "GroupChat11", 0),
+            GroupChat(12, "GroupChat12", 0),
+            GroupChat(13, "GroupChat13", 0),
+            GroupChat(14, "GroupChat14", 0),
+            GroupChat(15, "GroupChat15", 0),
+            GroupChat(16, "GroupChat16", 0),
         )
     }
 
