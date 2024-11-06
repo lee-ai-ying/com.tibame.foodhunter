5ae07852cdd606dd084e98755e43c108af3a4819
SharonMai <ppp300a@gmail.com>
2024-11-06 08:54:51 +0800

    feat: top bar 、tab共享狀態

--- app/src/main/java/com/tibame/foodhunter/sharon/PersonalToolsScreen.kt
+++ app/src/main/java/com/tibame/foodhunter/sharon/PersonalToolsScreen.kt
@@ -15,9 +15,6 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,22 +27,25 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.tibame.foodhunter.R
 import com.tibame.foodhunter.sharon.components.TabBarComponent
-import com.tibame.foodhunter.sharon.viewmodel.TabRowToolsViewModel
+import com.tibame.foodhunter.sharon.viewmodel.PersonalToolsVM
 import com.tibame.foodhunter.ui.theme.FoodHunterTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.tibame.foodhunter.sharon.components.topbar.CalendarTopBar
+import com.tibame.foodhunter.sharon.components.topbar.NoteTopBar
+import com.tibame.foodhunter.sharon.viewmodel.CalendarVM
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PersonalToolsScreen(
     navController: NavHostController,
-    viewModel: TabRowToolsViewModel = viewModel()
+    viewModel: PersonalToolsVM = viewModel()
 ) {
     // 獲取當前導航棧中的目的地，用於判斷是否顯示 TopBar 和返回按鈕
     val destination = navController.currentBackStackEntryAsState().value?.destination
-    // 當前選到的Tab
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     val uiState by viewModel.uiState.collectAsState()
+
 
 
     Scaffold(
@@ -53,6 +53,21 @@ fun PersonalToolsScreen(
             .fillMaxSize()  // 確保填滿
             .nestedScroll(scrollBehavior.nestedScrollConnection)
             .background(color = Color.White),
+        topBar = {
+            when (uiState.selectedTabIndex) {
+                TabConstants.CALENDAR -> CalendarTopBar(
+                    navController = navController,
+                    scrollBehavior = scrollBehavior,
+                    personalToolsVM = viewModel,
+                    calendarVM = CalendarVM()
+                )
+                TabConstants.NOTE -> NoteTopBar(
+                    navController = navController,
+                    scrollBehavior = scrollBehavior,
+                    personalToolsVM = viewModel
+                )
+            }
+        },
         floatingActionButton = {
             when (uiState.selectedTabIndex) {
                 in TabConstants.CALENDAR..TabConstants.NOTE  ->
@@ -69,14 +84,10 @@ fun PersonalToolsScreen(
                     }
             }
         },
-    ) { innerPadding -> // innerPadding 是 Scaffold 自動提供的內邊距，通常會包括 TopBar、BottomBar 的高度
+    ) { innerPadding ->
         Column(
             modifier = Modifier
-                // 只使用底部的 padding，忽略頂部的
-                .padding(
-                    bottom = innerPadding.calculateBottomPadding(),
-                    // top = 0.dp  // 頂部不需要 padding
-                )
+                .padding(innerPadding)
                 .fillMaxSize()
                 .background(color = Color.White)
         ) {
@@ -89,8 +100,20 @@ fun PersonalToolsScreen(
 
             // 根據選中的 Tab 顯示對應的頁面
             when (uiState.selectedTabIndex) {
-                TabConstants.CALENDAR -> CalendarScreen(navController)
-                TabConstants.NOTE -> NoteScreen(navController)
+                TabConstants.CALENDAR -> {
+
+                    CalendarScreen(
+                        navController = navController,
+                        viewModel = viewModel()
+                    )
+                }
+                TabConstants.NOTE -> {
+
+                    NoteScreen(
+                        navController = navController,
+                        noteVM = viewModel()
+                    )
+                }
             }
         }
     }
