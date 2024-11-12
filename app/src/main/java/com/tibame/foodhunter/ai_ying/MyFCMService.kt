6c13111d0fcca9e2cfca1f91f7ce472526254daf
MSI\Modern 14 <supaucat@gmail.com>
2024-11-12 15:48:51 +0800

    私訊功能串接FCM推播
    輸入框位置調整

--- app/src/main/java/com/tibame/foodhunter/ai_ying/MyFCMService.kt
+++ app/src/main/java/com/tibame/foodhunter/ai_ying/MyFCMService.kt
@@ -1,6 +1,7 @@
 package com.tibame.foodhunter.ai_ying
 
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -20,6 +21,8 @@ class MyFCMService : FirebaseMessagingService() {
         // 取得自訂資料
         val data = remoteMessage.data["data"]
         groupRepository.gChatVM?.updateGroupChat()
+        val userVM = groupRepository.gChatVM?.userVM
+        groupRepository.pChatVM?.refreshmessage2(userVM?.username?.value?:"", groupRepository.pChatVM?.friend?.value?:"")
     }
 
     // 當registration token更新時呼叫，應該將新的token傳送至server
